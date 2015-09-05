796b7caa2fbf2721333f7224896505fa17bab930
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2015-09-05 16:04:37 -0400

    updated with more variables

--- crowdfund.rb
+++ crowdfund.rb
@@ -1,7 +1,21 @@
 project1 = "Project ABC"
 project2 = "Project LMN"
 project3 = "Project XYZ"
+project4 = "Project IHO"
 
 funding1 = 1000
+funding2 = 2900
+funding3 = 1800
+funding4 = 3000
+
+pdate1 = "September 1, 2015"
+pdate2 = "September 3, 2015"
+pdate3 = "September 5, 2015"
+pdate4 = "September 5, 2015"
 
 puts "Projects: \n\t #{project1} \n\t #{project2} \n\t #{project3}"
+
+puts "#{project1.upcase} was launched on #{pdate1} and has $#{funding1} in funding!"
+puts "#{project2.upcase} was launched on #{pdate2} and has $#{funding2} in funding!"
+puts "#{project3.upcase} was launched on #{pdate3} and has $#{funding3} in funding!"
+puts "#{project4.upcase} was launched on #{pdate4} and has $#{funding4} in funding!"
